3e1a426fe7a49131e791e0ccb588172b71221ff7
Tony Han <itony9401@live.com>
2019-01-31 20:47:03 -0800

    added frontend react app

--- app/app/settings.py
+++ app/app/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'frontend',
     'backend',
     'rest_framework'
 ]
--- app/frontend/src/components/App.jsx
+++ app/frontend/src/components/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>React App</h1>;
+    return <h1>Hello World!</h1>;
   }
 }
 
