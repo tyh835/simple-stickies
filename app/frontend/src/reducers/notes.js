02486a38ffa3ec96936ed68899354ff4ea37927e
Tony Han <itony9401@live.com>
2019-02-01 00:38:52 -0800

    added actionTypes

--- app/frontend/src/reducers/notes.js
+++ app/frontend/src/reducers/notes.js
@@ -1,7 +1,19 @@
-const initialState = {};
+import { SET_NOTES } from '../actionTypes';
+
+const initialState = {
+  loading: false,
+  notes: [],
+  cachedNotes: []
+};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
+    case SET_NOTES:
+      return {
+        ...state,
+        notes: [...payload],
+        cachedNotes: [...payload]
+      };
     default:
       return state;
   }
