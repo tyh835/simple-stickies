56c60bb95588a480f0084de3ec67c0e7f32de51d
Tony Han <itony9401@live.com>
2019-01-31 22:34:52 -0800

    added frontend view test

--- app/frontend/tests.py
+++ app/frontend/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class FrontendTest(TestCase):
+    def test_index(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEquals(response.status_code, 200)
