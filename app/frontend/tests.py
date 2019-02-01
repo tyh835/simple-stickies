4c5d96f9abc04028c59ffa6aac3ae76d97699bae
Tony Han <itony9401@live.com>
2019-01-31 22:53:37 -0800

    added docstring

--- app/frontend/tests.py
+++ app/frontend/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase, Client
 
 class FrontendTest(TestCase):
     def test_index(self):
+        """Root path should return status 200"""
         client = Client()
         response = client.get('/')
         self.assertEquals(response.status_code, 200)
