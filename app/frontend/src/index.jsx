55674db37d9858b01b580b312d46bd92f98d1f7a
Tony Han <itony9401@live.com>
2019-02-01 00:21:27 -0800

    created rootReducer

--- app/frontend/src/index.jsx
+++ app/frontend/src/index.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
 import App from './components/App';
+import rootReducer from './reducers';
 import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
