1ba0f2b0eae99f32448f5bc4ae5922a51fca7314
Tony Han <itony9401@live.com>
2019-02-07 23:58:20 -0800

    fixed notes error handling

--- app/frontend/src/utils/notes.js
+++ app/frontend/src/utils/notes.js
@@ -3,11 +3,10 @@ import { AUTH_ERROR, LOADING_ERROR } from '../actionTypes';
 const UNAUTHORIZED = 'Sorry, you need to be logged in to perform this action.';
 
 export const handleNotesError = (dispatch, err) => {
-  if (err.response.status === 401) {
-    dispatch({ type: AUTH_ERROR, payload: UNAUTHORIZED });
-  } else {
-    dispatch({ type: LOADING_ERROR, payload: err.message });
+  if (err.response && err.response.status === 401) {
+    return dispatch({ type: AUTH_ERROR, payload: UNAUTHORIZED });
   }
+  dispatch({ type: LOADING_ERROR, payload: err.message });
 };
 
 export const noteHasChanges = (currentNote, cachedNote) => {
